96298a1ebfa02004b8317690dd9466d49d3e8622
kovalev_v <Vladkovalev0410@gmail.com>
2021-11-03 12:04:25 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
+import { TesterPageComponent } from './pages/tester-page/tester-page.component';
+import { HeaderComponent } from './component/header/header.component';
+import { ButtonComponent } from './elements/button/button.component';
+import { SignUpComponent } from './elements/sign-up/sign-up.component';
+import { ArrowComponent } from './elements/arrow/arrow.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    TesterPageComponent,
+    HeaderComponent,
+    ButtonComponent,
+    SignUpComponent,
+    ArrowComponent
   ],
   imports: [
     BrowserModule,
